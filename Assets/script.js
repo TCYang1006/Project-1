7d2583f22bf75633f94b0e3c1368e454743d8e13
TC Yang <yangtongchee@hotmail.com>
2021-09-21 20:20:40 -0400

    saving draft

--- Assets/script.js
+++ Assets/script.js
@@ -18,6 +18,19 @@ var endDate = moment().add(6, 'd').format();
     });
 }
  */
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([37.41, 8.82]),
+    zoom: 4
+  })
+});
+
 
 function getEvents(search) {
     var search = document.getElementById('search').value;
